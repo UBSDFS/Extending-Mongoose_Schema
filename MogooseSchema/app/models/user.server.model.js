4d6bb1a29e67aa50ea9fe8c7f23c47dc73a5d104
Ulysses <ulyssesburdeniii@gmail.com>
2026-05-24 15:13:31 -0400

    Extend mongoose schema with modifiers and defaults

---
+++ MogooseSchema/app/models/user.server.model.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    firstName: String,
+
+    lastName: String,
+
+    email: {
+        type: String,
+        index: true
+    },
+
+    username: {
+        type: String,
+        trim: true,
+        unique: true
+    },
+
+    password: String,
+
+    website: {
+        type: String,
+
+        get: function(url) {
+            if (!url) {
+                return url;
+            } else {
+                if (
+                    url.indexOf('http://') !== 0 &&
+                    url.indexOf('https://') !== 0
+                ) {
+                    url = 'http://' + url;
+                }
+
+                return url;
+            }
+        },
+
+        set: function(url) {
+            if (!url) {
+                return url;
+            } else {
+                if (
+                    url.indexOf('http://') !== 0 &&
+                    url.indexOf('https://') !== 0
+                ) {
+                    url = 'http://' + url;
+                }
+
+                return url;
+            }
+        }
+    },
+
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+UserSchema.virtual('fullName').get(function () {
+    return this.firstName + ' ' + this.lastName;
+});
+
+UserSchema.set('toJSON', {
+    getters: true,
+    virtuals: true
+});
+
+mongoose.model('User', UserSchema);
